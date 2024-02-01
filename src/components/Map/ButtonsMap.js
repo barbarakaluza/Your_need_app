a25d25142ca01972f121820f1c73a64e4487578d
Barbara <kaluzabarbara26@gmail.com>
2024-02-01 14:36:22 +0100

    creating the skeleton of the Map component

---
+++ src/components/Map/ButtonsMap.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ButtonsMap() {
+    return (
+        <div>
+            <button className="button-plus"><img/></button>
+            <button className="button-loupe"><img/></button>
+            <button className="button-minus"><img/></button>
+        </div>
+    )
+}
+
+export default ButtonsMap;
