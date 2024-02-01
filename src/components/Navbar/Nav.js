7185d240c81244e5fbd2620c756300724055e35d
Barbara <kaluzabarbara26@gmail.com>
2024-02-01 14:51:49 +0100

    creating the skeleton of the Navbar component

---
+++ src/components/Navbar/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Nav extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        <nav className="nav">
+            <ul>
+                <li className="nav-link"><a href="#">Home</a></li>
+                <li className="nav-link"><a href="#">About</a></li>
+                <li className="nav-link"><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    }
+}
+
+export default Nav;
