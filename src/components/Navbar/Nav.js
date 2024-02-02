9a0050e41e6ee4926fbf91f1272f263e64774638
Barbara <kaluzabarbara26@gmail.com>
2024-02-02 12:41:28 +0100

    implementing navbar and add changes to symbols

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -8,9 +8,13 @@ class Nav extends React.Component {
     render() {
         <nav className="nav">
             <ul>
-                <li className="nav-link"><a href="#">Home</a></li>
-                <li className="nav-link"><a href="#">About</a></li>
-                <li className="nav-link"><a href="#">Contact</a></li>
+                <li className="nav-link"><a href="#">My visits</a></li>
+                <li className="nav-link"><a href="#">My account</a></li>
+                <li className="nav-link"><a href="#">Subscription</a></li>
+                <li className="nav-link"><a href="#">My reviews</a></li>
+                <li className="nav-link"><a href="#">Log out</a></li>
+                <li className="nav-link"><a href="#">Support</a></li>
+
             </ul>
         </nav>
     }
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Logo from "../components/Logo/Logo";
+
+function Home() {
+    return (
+        <div>
+            <Logo/>
+        </div>
+    )
+}
+
+export default Home;
